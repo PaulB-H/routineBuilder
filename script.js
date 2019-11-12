dc0becf214ee996cca2029dcbdb80b8fba638d4f
Paul B-H <51964537+PaulB-H@users.noreply.github.com>
2019-11-12 18:09:44 -0500

    Completed secondary API call within getExercises to append the front and back images to an exercise, if they exist

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 $(document).ready(function () {
 
+    var iterator;
 
-
-    $( ".frontItem" ).click(function() {
+    $(".frontItem").click(function () {
         console.log(`${this.text} clicked!`);
         $(".selectedMuscle").val(`${this.text}`);
     });
 
 
-
-
     function getExercises() {
+
         let queryURL = `https://wger.de/api/v2/exercise/?language=2&muscles=1&status=2`;
 
         $.get(`${queryURL}`, function (response) {
@@ -18,9 +17,11 @@ $(document).ready(function () {
             console.log(response.results[0].category);
 
             for (var i = 0; i < 5; i++) {
+                let iterator = i;
+                console.log(iterator);
 
                 $(".workoutList").append(`
-                    <div class="workoutBlock">
+                    <div id="work${i}" class="workoutBlock">
                         <h1>${response.results[i].name}</h1>
                         <p>${response.results[i].description}</p>
                         <button>Add to routine</button>
@@ -30,22 +31,26 @@ $(document).ready(function () {
                 let queryURL = `https://wger.de/api/v2/exerciseimage/?exercise=${response.results[i].id}`
 
                 $.get(`${queryURL}`, function (response) {
+                    console.log(`iterator is ${iterator}`)
 
-                    if (response.count == 0){
-                        console.log("No image here");
-                    }
-
-                    else {
-                        console.log(response);
-                    }
+                        if (response.count == 2) {
+                            $(`#work${iterator}`).append(`
+                            <div>
+                                <img src="${response.results[0].image}">
+                                <img src="${response.results[1].image}">
+                            </div>
+                            `)
+                        }
+                        else {
+                            console.log("No image here");
+                        };
 
                 });
             }
         })
     };
 
-
-
+    // function getExerciseImages
 
 
     getExercises();
