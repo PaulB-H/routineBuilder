f72e9cee1541124b862bb963c9310198b50f1a8f
Paul B-H <51964537+PaulB-H@users.noreply.github.com>
2019-11-13 03:03:28 -0500

    added button to collapse/expand details, made workoutBlock's consistent in sizing, fixed issue with stretched images, fixed header display and linked header dropdown items to appropriate muscle ID's, removed test console.log's, added footer

--- script.js
+++ script.js
@@ -4,55 +4,62 @@ $(document).ready(function () {
 
     $(".frontItem").click(function () {
         console.log(`${this.text} clicked!`);
+        console.log(`The muscleID is: ${this.id}`);
         $(".selectedMuscle").val(`${this.text}`);
+        $(".selectedMuscle").attr('id', `${this.id}`);
     });
 
+    $(".rearItem").click(function () {
+        console.log(`${this.text} clicked!`);
+        console.log(`The muscleID is:${this.id}`);
+        $(".selectedMuscle").val(`${this.text}`);
+        $(".selectedMuscle").attr('id', `${this.id}`);
+    });
 
     function getExercises() {
-
         let queryURL = `https://wger.de/api/v2/exercise/?language=2&muscles=1&status=2`;
 
         $.get(`${queryURL}`, function (response) {
-            console.log(response);
-            console.log(response.results[0].category);
 
             for (var i = 0; i < 5; i++) {
                 let iterator = i;
-                console.log(iterator);
 
                 $(".workoutList").append(`
                     <div id="work${i}" class="workoutBlock">
-                        <h1>${response.results[i].name}</h1>
-                        <p>${response.results[i].description}</p>
+                        <h5>${response.results[i].name}</h5>
                         <button>Add to routine</button>
+                        <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo${i}">Tap for details</button>
+                        <div id="demo${i}" class="collapse">
+                        <p>${response.results[i].description}</p>
+                        <div id="imageHolder${i}"></div>
+                        </div>
+                        
                     </div>
                 `);
 
-                let queryURL = `https://wger.de/api/v2/exerciseimage/?exercise=${response.results[i].id}`
+                let queryURL = `https://wger.de/api/v2/exerciseimage/?exercise=${response.results[i].id}`;
 
                 $.get(`${queryURL}`, function (response) {
-                    console.log(`iterator is ${iterator}`)
-
-                        if (response.count == 2) {
-                            $(`#work${iterator}`).append(`
+                    if (response.count == 2) {
+                        $(`#imageHolder${iterator}`).append(`
                             <div>
                                 <img src="${response.results[0].image}">
                                 <img src="${response.results[1].image}">
                             </div>
                             `)
-                        }
-                        else {
-                            console.log("No image here");
-                        };
-
+                    }
+                    else {
+                        console.log("No image here");
+                        $(`#imageHolder${iterator}`).append(`
+                            <div>
+                            <p style="text-align: center;">Sorry, no image!</p>
+                            </div>
+                            `)
+                    };
                 });
             }
         })
     };
 
-    // function getExerciseImages
-
-
     getExercises();
-
 });
